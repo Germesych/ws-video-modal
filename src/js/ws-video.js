a6100cf1e295be0af2514cd4afe9c962283a8e92
alex <alex@192.168.1.6>
2022-06-24 03:05:09 +0300

    refactoring

---
+++ src/js/ws-video.js
@@ -0,0 +1,45 @@
+class WsVideo {
+  constructor(options) {
+    this.mainBlock = options.mainBlock
+    this.renderImg()
+  }
+  selectors = {
+    modal: document.querySelector('.ws-video__modal'),
+    modalClose: document.querySelector('.ws-video__close'),
+    videoBlock: document.querySelector('.ws-video__video-block'),
+  }
+  renderImg(){
+    let block = document.querySelectorAll(this.mainBlock)
+    block.forEach((item)=>{
+      let link = item.querySelector('.ws-video__link')
+      let img = item.querySelector('.ws-video__img')
+      let urlId = link.href.split("/").at(-1)
+      link.addEventListener('click', (event)=>{
+        event.preventDefault()
+        this.modalOpen(event.target.href)
+      })
+      img.src = `https://img.youtube.com/vi/${urlId}/hqdefault.jpg`
+    })
+  }
+  modalOpen(linkId){
+    this.selectors.modalClose.addEventListener('click', this.closeModal)
+    this.selectors.modal.style.display = 'flex'
+    this.addVideoFromModal(linkId.split("/").at(-1))
+    document.body.style.overflowY = 'hidden'
+  }
+  closeModal(event){
+    document.querySelector('.ws-video__modal').style.display = 'none'
+    document.querySelector('.iframe-wrap').remove()
+    document.querySelector('.ws-video__modal').removeEventListener('click', this.closeModal)
+    document.body.style.overflowY = ''
+  }
+  addVideoFromModal(linkId){
+    let div = document.createElement('div')
+    div.className = 'iframe-wrap'
+    div.innerHTML = `<iframe class="ws-modal__iframe" src="https://www.youtube.com/embed/${linkId || ""}" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`
+    this.selectors.videoBlock.append(div)
+  }
+}
+new WsVideo({
+  mainBlock: '.ws-video'
+})
---
+++ webpack.config.js
@@ -0,0 +1,82 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyPlugin = require("copy-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const autoprefixer = require('autoprefixer');
+
+module.exports = (env, argv) => {
+  const isProd = argv.mode === 'production'
+  const isDev = !isProd
+
+  const filename = ext => isProd ? `[name].[contenthash].bundle.${ext}` : `[name].bundle.${ext}`
+
+  console.log(argv.mode)
+
+  return {
+    target: "web",
+    context: path.resolve(__dirname, 'src'),
+    entry: {
+      main: [
+        'core-js/stable',
+        'regenerator-runtime/runtime',
+        './index.js'
+      ],
+    },
+    output: {
+      path: path.resolve(__dirname, 'dist'),
+      filename: filename('js'),
+      clean: true,
+    },
+    resolve: {
+      extensions: ['.js'],
+      alias: {
+        '@': path.resolve(__dirname, 'src'),
+        '@core': path.resolve(__dirname, 'src', 'core'),
+      }
+    },
+    devtool: isDev ? 'source-map' : false,
+    devServer: {
+      port: 3000,
+      // open: true,
+      hot: true,
+      watchFiles: './',
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: './index.html'
+      }),
+      new CopyPlugin({
+        patterns: [
+          { from: path.resolve(__dirname, 'src', 'favicon.ico'), to:  path.resolve(__dirname, 'dist')},
+          { from: path.resolve(__dirname, 'src/img/**/*',), to:  path.resolve(__dirname, 'dist')},
+        ],
+      }),
+      new MiniCssExtractPlugin({
+        filename: filename('css')
+      }),
+    ],
+    module: {
+      rules: [
+        {
+          test: /\.s[ac]ss$/i,
+          use: [
+            MiniCssExtractPlugin.loader,
+            "css-loader",
+            "postcss-loader",
+            "sass-loader",
+          ],
+        },
+        {
+          test: /\.m?js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: "babel-loader",
+            options: {
+              presets: ['@babel/preset-env']
+            }
+          }
+        },
+      ],
+    },
+  }
+}
